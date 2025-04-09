b0795cea90f46a75a2b1356ea4c1e258be3197ea
tacticalYP <164803053+tacticalYP@users.noreply.github.com>
2025-04-09 21:13:02 +0530

    Added readme for CBF

--- Models/ContentBasedFiltering/pickle_model/movie_recommender_utils.py
+++ Models/ContentBasedFiltering/pickle_model/movie_recommender_utils.py
@@ -1,9 +1,16 @@
 from scipy.spatial.distance import euclidean
 import numpy as np
 import pandas as pd
+import pickle
 
-def recommend_movies(movie_name, df, feature_matrix, expected_language=None, genre_filter=None, top_n=5):
-    
+with open("movie_recommender_model.pkl", "rb") as f:
+    model_data = pickle.load(f)
+
+df = model_data["df"]
+feature_matrix = model_data["feature_matrix"]
+
+
+def recommend_movies(movie_name, df, feature_matrix, top_n=10):
     
     if movie_name not in df['title'].values:
         return "Movie not found in dataset. Try another one."
@@ -15,27 +22,5 @@ def recommend_movies(movie_name, df, feature_matrix, expected_language=None, gen
     
     sorted_indices = np.argsort(distances)[1:]
     recommended_movies = df.iloc[sorted_indices]
-
-    # print(recommended_movies)
-    # Filter by expected language if provided
-    if expected_language:
-        recommended_movies = recommended_movies[recommended_movies['original_language'] == expected_language]
-
-    if genre_filter:
-        recommended_movies['matching_genres'] = recommended_movies['genres'].apply(
-            lambda genres: sum(1 for genre in genre_filter if genre in genres)
-        )
-        
-        recommended_movies = recommended_movies[recommended_movies['matching_genres'] > 0]
-        # print(recommended_movies[["genres", "title", "matching_genres"]])
-        
-        n_genre = len(genre_filter)
-        rec = pd.DataFrame([],columns=recommended_movies.columns)
-
-        for i in range(n_genre):
-            rec = pd.concat([rec,recommended_movies[recommended_movies["matching_genres"]==n_genre-i]])
-        # print(rec[["matching_genres", "title", "genres"]])
-
-        return rec['title'].head(top_n).to_list()
     
     return recommended_movies['title'].head(top_n).to_list()
